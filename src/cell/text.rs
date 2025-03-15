2ed7433fc773e41e2115a5a12b91254df8e32893
Shiney <shiney618@proton.me>
2025-03-15 18:59:46 +0300

    `Option<NonMaxU32>` -> `MaybeImageIndex`

--- src/cell/text.rs
+++ src/cell/text.rs
@@ -4,18 +4,37 @@ use {
         Time,
     },
     crate::DocumentReader,
-    nonmax::NonMaxU32,
-    std::{
-        io,
-        io::Read,
+    std::io::{
+        self,
+        Read,
     },
 };
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct MaybeImageIndex(u32);
+
+impl MaybeImageIndex {
+    pub fn read<R>(reader: &mut DocumentReader<R>) -> io::Result<Self>
+    where
+        R: Read,
+    {
+        reader.read_u32().map(Self)
+    }
+
+    pub const fn is_some(self) -> bool {
+        self.0 != u32::MAX
+    }
+
+    pub fn into_index(self) -> Option<u32> {
+        self.is_some().then_some(self.0)
+    }
+}
 
 #[derive(Debug)]
 pub struct CellText {
     pub text: String,
     pub relative_size: i32,
-    pub image_index: Option<NonMaxU32>,
+    pub image_index: MaybeImageIndex,
     pub style: TextStyle,
     pub last_edit: Time,
 }
@@ -27,7 +46,7 @@ impl CellText {
     {
         let text = reader.read_string()?;
         let relative_size = -reader.read_i32()?;
-        let image_index = NonMaxU32::new(reader.read_u32()?);
+        let image_index = MaybeImageIndex::read(reader)?;
         let style = TextStyle::from_bits(reader.read_u32()?)
             .ok_or_else(|| io::Error::other("unknown style bits"))?;
         let last_edit = Time::read(reader)?;
